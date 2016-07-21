59e6213f69c429d90b9be9b77c149094d54163c1
Sly Verano <slyverano@gmail.com>
2016-07-21 12:05:15 +0800

    DEV: Change the name of the application from Swap to Ves

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for current version of Capistrano
 lock '3.5.0'
 
-set :application, 'swap'
-set :repo_url, 'git@github.com:scverano/swap.git'
+set :application, 'ves'
+set :repo_url, 'git@github.com:scverano/ves.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/deploy/apps/swap'
+set :deploy_to, '/home/deploy/apps/ves'
 
 # Default value for :scm is :git
 # set :scm, :git
